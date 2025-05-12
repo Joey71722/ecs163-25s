21b03d56c33412bdd7ac4145b67fb389799b496c
Joanne Lai <joannelai@campus-035-013.ucdavis.edu>
2025-05-12 16:10:58 -0700

    Homework2

--- Homework2/Joey7172/main.js
+++ Homework2/Joey7172/main.js
@@ -2,6 +2,7 @@ const margin = { top: 50, right: 30, bottom: 50, left: 60 };
 const width = 800 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
+
 d3.csv("data/pokemon.csv").then(data => {
   // Parse numeric values
   data.forEach(d => {
